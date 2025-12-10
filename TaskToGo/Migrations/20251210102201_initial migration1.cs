2c0e2458dac734f8502f7b0c473dff07aaeb15d2
Raya Al Amri <rayaalamri90@gmail.com>
2025-12-10 14:36:26 +0400

    Edit design

---
+++ TaskToGo/Migrations/20251210102201_initial migration1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskToGo.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2025, 12, 10, 10, 22, 1, 231, DateTimeKind.Utc).AddTicks(6008))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "todoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCompeleted = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2025, 12, 10, 10, 22, 1, 231, DateTimeKind.Utc).AddTicks(8190)),
+                    TaskCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_todoTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_todoTasks_TaskCategories_TaskCategoryId",
+                        column: x => x.TaskCategoryId,
+                        principalTable: "TaskCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_todoTasks_TaskCategoryId",
+                table: "todoTasks",
+                column: "TaskCategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todoTasks");
+
+            migrationBuilder.DropTable(
+                name: "TaskCategories");
+        }
+    }
+}
---
+++ TaskToGo/wwwroot/js/custom.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#menuToggle").click(function () {
+        $(".sidebar").toggleClass("collapsed");
+        if ($(".sidebar").hasClass("collapsed")) {
+            $(".main-content").css("margin-left", "0");
+        } else {
+            $(".main-content").css("margin-left", "250px");
+        }
+    });
+});
