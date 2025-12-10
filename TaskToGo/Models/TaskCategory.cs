559c9058b189a14b696cdddb8ed1ebf43d2ecb83
Raya Al Amri <rayaalamri90@gmail.com>
2025-12-10 20:33:41 +0400

    Edit

--- TaskToGo/Models/TaskCategory.cs
+++ TaskToGo/Models/TaskCategory.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
 
         public IEnumerable<TodoTask> tasks { get; set; } = new HashSet<TodoTask>();
